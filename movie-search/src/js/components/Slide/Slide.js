3949e8caf22f4b5f3fc32ae084ba33a5824a9427
Maksim Pavlov <pavlovmark100@gmail.com>
2020-05-01 00:16:47 +0400

    feat: add preload

--- movie-search/src/js/components/Slide/Slide.js
+++ movie-search/src/js/components/Slide/Slide.js
@@ -20,6 +20,7 @@ export default class Slide {
 
   async getMovies() {
     const url = `https://www.omdbapi.com/?s=${this.inputValue}&apikey=9b67fc54`;
+    this.preload();
     const res = await fetch(url);
     const data = await res.json();
     // console.log(this.inputValue);
@@ -36,13 +37,21 @@ export default class Slide {
   async getTitle(data) {
     this.slides.forEach((slide, index) => {
       slide.innerHTML = '';
-      // slide.insertAdjacentHTML('afterbegin', `<a class="slide__header">${data.Search[index].Title}</a>`);
-      // slide.insertAdjacentHTML('beforeend', `<img class="slide__poster" src="${data.Search[index].Poster}">`);
-      // slide.insertAdjacentHTML('beforeend', `<div class="slide__year">${data.Search[index].Year}</div>`);
-      slide.insertAdjacentHTML('afterbegin', `<div class="slide__title"><a class="slide__title_link" href="#">feefef</a></div>`);
-      slide.insertAdjacentHTML('beforeend', `<div class ="slide__poster"><img class="slide__poster_img" src="https://sun9-7.userapi.com/c857732/v857732544/13dbc5/ldsw21Z1SYQ.jpg"></div>`);
-      slide.insertAdjacentHTML('beforeend', `<div class="slide__year">1998</div>`);
+      slide.insertAdjacentHTML('afterbegin', `<a class="slide__header">${data.Search[index].Title}</a>`);
+      slide.insertAdjacentHTML('beforeend', `<img class="slide__poster" src="${data.Search[index].Poster}">`);
+      slide.insertAdjacentHTML('beforeend', `<div class="slide__year">${data.Search[index].Year}</div>`);
+      // slide.insertAdjacentHTML('afterbegin', `<div class="slide__title"><a class="slide__title_link" href="#">feefef</a></div>`);
+      // slide.insertAdjacentHTML('beforeend', `<div class ="slide__poster"><img class="slide__poster_img" src="https://sun9-7.userapi.com/c857732/v857732544/13dbc5/ldsw21Z1SYQ.jpg"></div>`);
+      // slide.insertAdjacentHTML('beforeend', `<div class="slide__year">1998</div>`);
     });
   }
 
+  preload() {
+    this.slides.forEach((slide) => {
+      slide.innerHTML = '';
+      slide.insertAdjacentHTML('afterbegin', `<div class="cssload-container">
+                                                            <div class="cssload-speeding-wheel"></div>
+                                                         </div>`);
+    });
+  }
 }
